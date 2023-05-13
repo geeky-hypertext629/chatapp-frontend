e9ffb5806ae70077f066c006838c136b1e8ad887
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-05-13 19:06:10 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from "socket.io-client";
+import Chat from './Chat';
+import "./App.css";
+
+const socket = io.connect('https://chatapp2-lk2w.onrender.com');
 
 function App() {
+
+  const [username, setusername] = useState("");
+  const [room, setRoom] = useState("");
+  const [userJoined,setUserJoined] = useState(false);
+
+  const joinUser = () => {
+    if (room !== "" && username !== "") {
+      socket.emit('join_room', { username, room });
+      setUserJoined(true);
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    !userJoined?(
+
+      <div className="login-box">
+  <h2>Join A Chat</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" placeholder='Enter Your Name' onChange={(e) => {
+        setusername(e.target.value);
+      }} value={username} />
+      <label>Enter Your Name</label>
     </div>
-  );
+    <div class="user-box">
+      <input type="text" placeholder='Enter room id' value={room} onChange={(e) => {
+        setRoom(e.target.value);
+      }} />
+      <label>Room Id</label>
+    </div>
+    <button onClick={joinUser} >
+    {/* <button > */}
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Join
+      {/* </button> */}
+    </button>
+  </form>
+</div>
+
+
+
+    // <div className='App'>
+    // <div className="joinChatContainer">
+    // <h3>Join A chat</h3>
+    //   <input type="text" placeholder='Enter Your Name' onChange={(e) => {
+    //     setusername(e.target.value);
+    //   }} value={username} />
+    //   <br />
+    //   <input type="text" placeholder='Enter room id' value={room} onChange={(e) => {
+    //     setRoom(e.target.value);
+    //   }} /> <br />
+    //   <button onClick={joinUser}>Submit</button>
+    //   </div>
+      // </div>
+      ):(
+      <div>
+      <Chat socket={socket} username={username} room={room}/>
+    </div>)
+
+  )
 }
 
 export default App;
---
+++ src/Chat.jsx
@@ -0,0 +1,92 @@
+import React, { useState,useEffect } from 'react';
+import "./App.css";
+import ScrollToBottom from "react-scroll-to-bottom";
+import { useToast } from '@chakra-ui/react'
+
+
+function Chat({socket,username,room}) {
+
+    useEffect(()=>{
+        triggerToast();
+    },[]);
+
+    const [currentMessage,setCurrentMessage] = useState("");
+    const [messageList, setmessageList] = useState([])
+
+    const sendMessage = async ()=>{
+        if(currentMessage!=="")
+        {
+            const messageData = {
+                room : room,
+                author : username,
+                message : currentMessage,
+                time : new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+
+            await socket.emit("send_message", messageData);
+            setmessageList((list)=>[...list,messageData])
+            setCurrentMessage("");
+        }
+    }
+
+    const toast = useToast();
+
+    const triggerToast = () => {
+        toast({
+            title: 'Account created.',
+            description: "We've created your account for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+    }
+
+    useEffect(()=>{
+        socket.on("self_message",(data)=>{
+            setmessageList(data);
+        })
+        socket.on("receive_message",(data)=>{
+            setmessageList((list)=>[...list,data])
+        })
+
+
+        return () => socket.removeListener('receive_message');
+      },[socket])
+
+
+  return (
+    <div className='chat-window'>
+    <div className="chat-header"> <p> Live Chat </p>
+    </div>
+    <div className="chat-body">
+    <ScrollToBottom className='message-container' >
+        {messageList.map((messageContent)=>{
+            return (
+                <div className="message" id={username===messageContent.author?"you" : "other"}>
+                    <div>
+                        <div className="message-content">
+                            <p>{messageContent.message}</p>
+                        </div>
+                        <div className="message-meta">
+                            <p id='time'>
+                                {messageContent.time}
+                            </p>
+                            <p id='author'>
+                                {messageContent.author}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
+        </ScrollToBottom>
+    </div>
+    <div className="chat-footer">
+        <input onKeyDown={(e)=>{e.key==='Enter' && sendMessage()}} type="text" placeholder='hey...' value={currentMessage} onChange={(e)=>{setCurrentMessage(e.target.value)}}/>
+        <button onClick={sendMessage}>&#9658;</button>
+    </div>
+    </div>
+  )
+}
+
+export default Chat;
